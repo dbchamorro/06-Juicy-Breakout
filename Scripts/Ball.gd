2e91243de08e4db1d22a94e1041b05e894070a1e
Destiny C <dbchamorro@yahoo.com>
2020-02-28 23:29:19 -0500

    3/5 Done
    3/5ths of the way done, music and other minuture effects need to be added.

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -2,20 +2,105 @@ extends RigidBody2D
 
 onready var Game = get_node("/root/Game")
 onready var Starting = get_node("/root/Game/Starting")
+onready var Comet = get_node("/root/Game/Comet")
+
+var _decay_rate = 0.8
+var _max_offset = 4
+var trauma_color = Color(1,1,1,1)
+
+var _start_size 
+var _start_position
+var _trauma = 0.0
+var _rotation = 0
+var _rotation_speed = 0.05
+var _color = 0.0
+var _color_decay = 1
+var _normal_color
+
+var _size_decay = 0.02
+var _alpha_decay = 0.03
+
+var _count = 0
 
 func _ready():
 	contact_monitor = true
 	set_max_contacts_reported(4)
+	_start_position = $ColorRect.rect_position
+	#_start_size =$ColorRect.get_transform().size
+	_normal_color =$ColorRect.color
 
+func _process(delta):
+	if _trauma > 0:
+		_decay_trauma(delta)
+		_apply_shake()
+	if _color > 0:
+		_decay_color(delta)
+		#_apply_color()
+	if _color == 0 and $ColorRect.color != _normal_color:
+		$ColorRect.color = _normal_color
+
+#create contrail
+	var temp = $ColorRect.duplicate()
+	temp.rect_position = Vector2(position.x + $ColorRect.rect_position.x, position.y + $ColorRect.rect_position.y)
+	temp.name = "Trail" + str(_count)
+	_count += 1
+	temp.color = temp.color.linear_interpolate(Color(0,0,0,1), 0.5)
+	Comet.add_child(temp)
+	var trail = Comet.get_children()
+	for t in trail:
+		t.rect_size = Vector2(t.rect_size.x - _size_decay, t.rect_size.y - _size_decay)
+		t.color.a -= _alpha_decay
+		if t.color.a <= 0:
+			t.color.a = 0
+		if t.rect_size.x <= 0.5 or t.color.a <= 0:
+			t.queue_free()
+
+
+# warning-ignore:unused_argument
 func _physics_process(delta):
 	# Check for collisions
 	var bodies = get_colliding_bodies()
 	for body in bodies:
+		#add_trauma(2.0)
 		if body.is_in_group("Tiles"):
 			Game.change_score(body.points)
-			body.queue_free()
-	
+			#add_color(1.0)
+			body.find_node("Smoke").emitting = true
+			body.kill()
+		if body.name == "Paddle":
+			var tile_row = get_tree().get_nodes_in_group("Tile Row")
+			for tile in tile_row:
+				tile.add_trauma(0.5)
+
 	if position.y > get_viewport().size.y:
 		Game.change_lives(-1)
 		Starting.startCountdown(3)
 		queue_free()
+
+func _add_color(amount):
+	_color += amount
+
+# warning-ignore:unused_argument
+func _apply_color(delta):
+	var a = min(1, _color)
+	$ColorRect.color = _normal_color.linear_interpolate(trauma_color, a)
+
+func _decay_color(delta):
+	var change = _color_decay * delta
+	_color = max(_color - change, 0)
+
+func _add_trauma(amount):
+	_trauma = min(_trauma + amount, 1)
+
+func  _decay_trauma(delta):
+	var change = _decay_rate * delta
+	_trauma = max(_trauma - change, 0)
+
+func _apply_shake():
+	var shake = _trauma * _trauma
+	var o_x = _max_offset * shake * _get_neg_or_pos_scaler()
+	var o_y = _max_offset * shake * _get_neg_or_pos_scaler()
+	$ColorRect.rect_position = _start_position + Vector2(o_x, o_y)
+
+func _get_neg_or_pos_scaler():
+	return rand_range(-1.0, 1.0)
--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -3,8 +3,35 @@ extends StaticBody2D
 var points = 10
 onready var _target = position
 
+var _dying = 0.0
+var _dying_delta = 0.1
+var _dying_color_delta = 0.05
+var _dying_rotate = 0.0
+var _dying_roatate_delta = rand_range(-0.03, 0.03)
+var _dying_threshold = 10
+
+onready var t = $Timer
+
 func _ready():
 	position.y = -30
+	var time = rand_range(0, 2.5)
+	t.set_wait_time(time)
+	t.start()
+	yield(t, "timeout")
 	$Tween.interpolate_property(self, "position", position, _target, 2.0, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
 	$Tween.start()
-	#6:46
+	
+
+func _process(delta):
+	if _dying > 0:
+		_dying += _dying_delta
+		position.y += _dying 
+		rotate(_dying_rotate)
+		_dying_rotate += _dying_roatate_delta
+		$ColorRect.color = $ColorRect.color.linear_interpolate(Color(0,0,0,0), _dying_color_delta)
+	if _dying > _dying_threshold:
+		queue_free()
+
+func kill():
+	_dying = _dying_delta
+	$CollisionShape2D.queue_free()
